543593bda4a15a2988e2f680a24e71f4c0988877
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-12 22:18:51 +0700

    Question 1 | create react app with typescript from scratch

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
+
+ReactDOM.render(
+  <h1>Hello React!</h1>,
+  document.getElementById('app-root'),
+)
