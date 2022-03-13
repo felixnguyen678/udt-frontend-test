f4b12947e0e35c252f1a1e439e21dd34b2382bfe
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-13 13:35:48 +0700

    Question 3 | WIP: setup SCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+// import {hot} from "react-hot-loader";
+// import "../styles/Home.module.css"
 const ReactDOMServer = require("react-dom/server");
 
 const Home = () => {
-  return <>Hello world</>;
+  return (
+    <div className="c-calculator">
+      <div className="c-calculator__screen">
+        <span className="c-screen__number">0</span>
+      </div>
+      <div className="c-calculator__keypad">
+        <button className="c-calculator__button--third">AC</button>
+        <button className="c-calculator__button--third">+/-</button>
+        <button className="c-calculator__button--third">%</button>
+        <button className="c-calculator__button--secondary">/</button>
+        <button className="c-calculator__button">7</button>
+        <button className="c-calculator__button">8</button>
+        <button className="c-calculator__button">9</button>
+        <button className="c-calculator__button--secondary">*</button>
+        <button className="c-calculator__button">4</button>
+        <button className="c-calculator__button">5</button>
+        <button className="c-calculator__button">6</button>
+        <button className="c-calculator__button--secondary">-</button>
+        <button className="c-calculator__button">1</button>
+        <button className="c-calculator__button">2</button>
+        <button className="c-calculator__button">3</button>
+        <button className="c-calculator__button--secondary">+</button>
+        <button className="c-calculator__button--large">0</button>
+        <button className="c-calculator__button">,</button>
+        <button className="c-calculator__button--secondary">=</button>
+      </div>
+    </div>
+  );
 };
 
 const HomeHTML: string = ReactDOMServer.renderToString(<Home />);
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
